e7f8f63a9529124a4184bd0e53f7761ff8cfa991
Ankur Sheel <ankur.sheel@gmail.com>
2020-01-03 12:03:20 +1300

    feat: add css for highlighting lines in code snippets

--- src/03-composites/Content.tsx
+++ src/03-composites/Content.tsx
@@ -18,14 +18,47 @@ const styles = {
         '& > p': {
             margin: '1em 0 0 0',
         },
-        ".gatsby-highlight pre[class*='language-'].line-numbers": {
-            padding: 0,
-            paddingLeft: '2.8em',
-        },
         figcaption: {
             textAlign: 'center',
             fontSize: '0.8em',
             fontStyle: 'italic',
+        },
+        '.gatsby-highlight-code-line': {
+            backgroundColor: '#707070',
+            display: 'block',
+            alignItems: 'center',
+            marginLeft: '-1em',
+            marginRight: '-1em',
+            paddingLeft: '0.75em',
+            paddingRight: '1em',
+            borderLeft: '0.25em solid #f99',
+        },
+        /**
+         * Add back the container background-color, border-radius, padding, margin
+         * and overflow that we removed from <pre>.
+         */
+        '.gatsby-highlight': {
+            borderRadius: '0.3em',
+            margin: '0.5em 0',
+            padding: '1em',
+            overflow: ' auto',
+        },
+        // /**
+        //  * Remove the default PrismJS theme background-color, border-radius, margin,
+        //  * padding and overflow.
+        //  * 1. Make the element just wide enough to fit its content.
+        //  * 2. Always fill the visible space in .gatsby-highlight.
+        //  * 3. Adjust the position of the line numbers
+        //  */
+        '.gatsby-highlight pre[class*="language-"]': {
+            margin: 0,
+            padding: 0,
+            overflow: 'initial',
+            float: 'left',
+            minWidth: '100%',
+        },
+        ".gatsby-highlight pre[class*='language-'].line-numbers": {
+            paddingLeft: '2.8em',
         },
     }),
 };
