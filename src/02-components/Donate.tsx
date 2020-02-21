ab0c17013fcd424548c8ea46797458cea1f7a9f9
Ankur Sheel <ankur.sheel@gmail.com>
2020-02-21 23:17:30 +1300

    chore: update styling

--- src/02-components/Donate.tsx
+++ src/02-components/Donate.tsx
@@ -1,14 +1,15 @@
 import Img from 'gatsby-image';
 import React from 'react';
 import useSiteImages from '../hooks/use-site-images';
-import { Styles } from '../tokens';
+import { Styles, theme } from '../tokens';
 
 const styles: Styles = {
     container: {
         display: 'flex',
         alignItems: 'center',
-        borderTop: 'solid 1px white',
-        padding: '1em',
+        borderTop: `solid 2px ${theme.colors.subTitle}`,
+        marginTop: '2rem',
+        paddingTop: '1em',
         fontSize: '1.5rem',
         flexWrap: 'wrap',
     },
@@ -24,7 +25,7 @@ const Donate = () => {
     const image = useSiteImages('coffee.png');
     return (
         <div css={styles.container}>
-            <span css={styles.label}>Enjoy this Blog?</span>
+            <span css={styles.label}>Enjoy this Article?</span>
             <a href="https://www.buymeacoffee.com/ankursheel" target="_blank" rel="noopener noreferrer">
                 <Img fluid={image?.fluid} alt="Buy Me A Coffee" style={styles.image} />
             </a>
