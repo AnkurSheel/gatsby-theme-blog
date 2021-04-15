b5e8047cf04d4e2d435f3334dfaa8f08c8bc9bc2
Ankur Sheel <ankur.sheel@gmail.com>
2021-04-15 17:24:19 +1200

    build: disable no-use-before-define rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
     extends: ['@codinators/eslint-config'],
+    rules: {
+        'jsx-a11y/label-has-associated-control': [
+            2,
+            {
+                labelAttributes: ['label'],
+                depth: 3,
+            },
+        ],
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error'],
+    },
 };
